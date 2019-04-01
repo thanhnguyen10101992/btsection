61a2e4b7eb388c75e4fd5b1afaf3ff9132649a0f
nguyenchithanh <nguyenchithanh10101992@gmail.com>
2019-04-01 16:53:11 +0700

    update

--- js/diemtrungbinh.js
+++ js/diemtrungbinh.js
@@ -1,56 +1,56 @@
 $(document).ready(function(){
   $("#tinhdiemtb").click(function(){
-    var a=parseInt($("#diemtoan").val());
-    var b=parseInt($("#diemly").val());
-    var c=parseInt($("#diemhoa").val());
+    var a=Number($("#diemtoan").val());
+    var b=Number($("#diemly").val());
+    var c=Number($("#diemhoa").val());
     var dtb=(a+b+c)/3;
     console.log('ok23',dtb)
     alert(dtb);
   });
   $("#chuyennd").click(function(){
-    var a=parseInt($("#doc").val());
+    var a=Number($("#doc").val());
     var dof=(9/5)*a+32
     console.log('ok23',dof)
     alert(a+'C'+'='+  dof+'F');
   });
    $("#tinhstron").click(function(){
-    var a=parseInt($("#bankinh").val());
+    var a=Number($("#bankinh").val());
     var Stron=a*a*Math.PI
     console.log('ok23',Stron)
     alert('Diện tích hình tròn la'+ Stron);
   });
 
    $('#convert').click(function(){
-   	var a=parseInt($("#amount").val());
+   	var a=Number($("#amount").val());
    	var b=a/23000;
    	alert('Số tiền'+ b +'USD');
    })
 
    $('#cong').click(function(){
-      var so1=parseInt($("#so1").val());
-      var so2=parseInt($("#so2").val());
+      var so1=Number($("#so1").val());
+      var so2=Number($("#so2").val());
       var tong=so1+so2;
       alert ('Tổng 2 số là'+ ' '+ tong)
    })
 
 
    $('#tru').click(function(){
-      var so1=parseInt($("#so1").val());
-      var so2=parseInt($("#so2").val());
+      var so1=Number($("#so1").val());
+      var so2=Number($("#so2").val());
       var hieu=so1-so2;
       alert ('Hiệu 2 số là'+ ' '+ hieu)
    })
 
    $('#nhan').click(function(){
-      var so1=parseInt($("#so1").val());
-      var so2=parseInt($("#so2").val());
+      var so1=Number($("#so1").val());
+      var so2=Number($("#so2").val());
       var nhan=so1*so2;
       alert ('Tích 2 số là'+ ' '+ nhan)
    })
 
    $('#chia').click(function(){
-      var so1=parseInt($("#so1").val());
-      var so2=parseInt($("#so2").val());
+      var so1=Number($("#so1").val());
+      var so2=Number($("#so2").val());
       var thuong=so1/so2;
       alert ('Thương 2 số là'+ ' '+ thuong)
    })
@@ -99,6 +99,24 @@ $(document).ready(function(){
 
     alert('tháng'+ ' '+ thang +' ' +'có' +' '+ day);
 
+   })
+
+   $('#tinhchisoct').click(function(){
+      var cn=Number($("#cn").val());
+      var cc=Number($("#cc").val());
+      var bmi=(cn)/(cc*cc);
+      if(bmi<18.5){
+        alert ('Underweight')
+      }
+      if(bmi>=18.5&& bmi<25){
+        alert('normal')
+      }
+      if(bmi>=25&&bmi<30){
+        alert('Overweight')
+      }
+      if(bmi>30){
+        alert('Obese')
+      }
    })
 
 });	
